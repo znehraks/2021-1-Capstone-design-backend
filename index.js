27007808767f611c44350992e120ea0d70bc5ebb
designC <znehraks@gmail.com>
2021-05-17 17:50:46 +0900

    cors

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const { spawn } = require("child_process");
+const cors = require("cors");
 const app = express();
 const port = 5000;
 //크롤링 및 분석 후에 최종 res.send로 프론트에 전달해야 함.
 //json parsing하는데에 변수가 많음.
 
+app.use(cors());
 // "Origin, X-Requested-With, Content-Type, Accept"
-
+app.all("/*", function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
@@ -20,9 +26,6 @@ app.use(function (req, res, next) {
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-String.prototype.replaceAll = function (org, dest) {
-  return this.split(org).join(dest);
-};
 app.post("/recommendation", (req, res) => {
   try {
     var data;
@@ -48,17 +51,15 @@ app.post("/recommendation", (req, res) => {
       // console.log("Pipe data from python script ...");
       // largeDataset.push(data);
 
-      data = chunk.toString("utf8");
-      data = data.replaceAll("'", '"');
-      data = data.replaceAll("None", '"None"');
+      data = chunk.toString("utf-8");
+      res.json(data);
     });
 
     // in close event we are sure that stream from child process is closed
     python.on("close", (code) => {
       console.log(`child process close all stdio with code ${code}`);
       // send data to browser
-      const obj = JSON.parse(data);
-      res.json(obj);
+      // const obj = JSON.parse(data);
     });
   } catch (e) {}
 });
